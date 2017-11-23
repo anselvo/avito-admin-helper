fcaa5d45708bc1815ad5ffe99044d048a3ae4137
Andrey Sergeev <asergeev@avito.ru>
2017-11-23 17:34:14 +0300

    add stock and stomp js libs

--- background.js
+++ background.js
@@ -660,3 +660,19 @@ function notificationCheck() {
 		}
 	});
 }
+
+
+let sock = new SockJS('https://mydomain.com/my_prefix');
+sock.onopen = function() {
+    console.log('open');
+    sock.send('test');
+};
+
+sock.onmessage = function(e) {
+    console.log('message', e.data);
+    sock.close();
+};
+
+sock.onclose = function() {
+    console.log('close');
+};
