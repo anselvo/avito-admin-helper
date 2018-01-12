67d008b3f69e5b67771c50321a869363d2c2457d
Roman Konkin <rkonkin@avito.ru>
2018-01-12 12:03:34 +0300

    add adm url patterns

--- content/main.js
+++ content/main.js
@@ -3,6 +3,25 @@ let scriptGlobal;
 let currentUrl;
 let springUrl;
 let admUrl;
+const admUrlPatterns = {
+    main: /https:\/\/adm\.avito\.ru\/$/,
+    items_search: /https:\/\/adm\.avito\.ru\/(?:adm\/)?items\/search/,
+    items_item_info: /https:\/\/adm\.avito\.ru\/\d+(?!\/)\b|(?:(?:adm\/)?items\/item\/info)/,
+    items_comparison: /https:\/\/adm\.avito\.ru\/(?:adm\/)?items\/comparison(?!\/\d+\/archive)/,
+    items_comparison_archive: /https:\/\/adm\.avito\.ru\/(?:adm\/)?items\/comparison\/\d+\/archive/,
+    items_moder: /https:\/\/adm\.avito\.ru\/(?:adm\/)?items\/moder/,
+    users_search: /https:\/\/adm\.avito\.ru\/(?:adm\/)?users\/search/,
+    users_user_info: /https:\/\/adm\.avito\.ru\/(?:\d+u(?!\/)\b)|(?:(?:adm\/)?users\/user\/info)/,
+    users_account_info: /https:\/\/adm\.avito\.ru\/(?:adm\/)?users\/account\/info/,
+    billing_walletlog: /https:\/\/adm\.avito\.ru\/(?:adm\/)?billing\/walletlog/,
+    billing_invoices: /https:\/\/adm\.avito\.ru\/(?:adm\/)?billing\/invoices/,
+    shops_info_view: /https:\/\/adm\.avito\.ru\/(?:adm\/)?shops\/info\/view/,
+    shops_moderation: /https:\/\/adm\.avito\.ru\/(?:adm\/)?shops\/moderation/
+    helpdesk: /https:\/\/adm\.avito\.ru\/helpdesk/,
+    system_access: /https:\/\/adm\.avito\.ru\/(?:adm\/)?system\/access/,
+    detectives_queue_search: /https:\/\/adm\.avito\.ru\/(?:adm\/)?detectives\/queue\/search/,
+    messenger_user: /https:\/\/adm\.avito\.ru\/(?:adm\/)?messenger\/user/
+};
 
 $(function () {
     currentUrl = location.href;
